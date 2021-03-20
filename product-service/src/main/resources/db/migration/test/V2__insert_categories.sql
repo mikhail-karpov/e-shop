75c4a8cdf7ea1dd87cc2387553a008c666b52cab
Mikhail Karpov <mikhailkarpov@hotmail.com>
2021-03-20 23:50:46 +0200

    Fixed tests

--- product-service/src/main/resources/db/migration/test/V2__insert_categories.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO category(id, title, description, parent_id) VALUES
-    (1, 'Electronics', 'Electronics & Computers', NULL),
-    (2, 'Software', 'Software and games', NULL),
-    (3, 'Computers', 'Computers and peripherals', 1),
-    (4, 'Camera & Photo', 'Camera, photo, accessories and other equipment', 1),
-    (5, 'Antivirus', 'Antivirus and security', 2),
-    (6, 'Business', 'Business and office', 2);
--- product-service/src/test/java/com/mikhailkarpov/eshop/productservice/AbstractIntegrationTest.java
+++ product-service/src/test/java/com/mikhailkarpov/eshop/productservice/AbstractIntegrationTest.java
@@ -1,16 +1,12 @@
 package com.mikhailkarpov.eshop.productservice;
 
-import org.junit.jupiter.api.AfterAll;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 public class AbstractIntegrationTest {
 
     static final PostgreSQLContainer postgres;
-    static final GenericContainer config;
-    static final GenericContainer eureka;
 
     static {
         postgres = (PostgreSQLContainer) new PostgreSQLContainer("postgres:12-alpine")
@@ -19,15 +15,7 @@ public class AbstractIntegrationTest {
                 .withPassword("password")
                 .withReuse(true);
 
-        config = new GenericContainer("springcloud/configserver")
-                .withReuse(true);
-
-        eureka = new GenericContainer("springcloud/eureka")
-                .withReuse(true);
-
         postgres.start();
-        config.start();
-        eureka.start();
     }
 
     @DynamicPropertySource
